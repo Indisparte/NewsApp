f5e3db3bbdb691daee60e71494bc23b8ba0c1675
Antonio <tonydinu02@gmail.com>
2023-04-03 16:16:02 +0200

    Utlimato il modulo feature:search
    - Rimosso definitivamente il modulo news:search_news

---
+++ feature/search/src/main/java/com/indisparte/search/adapter/BindingAdapters.kt
@@ -0,0 +1,17 @@
+package com.indisparte.search.adapter
+
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.databinding.BindingAdapter
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+
+@BindingAdapter("loadImage")
+fun loadImage(imageView: AppCompatImageView, url: String) {
+
+    val circularProgressDrawable = CircularProgressDrawable(imageView.context)
+    circularProgressDrawable.strokeWidth = 5f
+    circularProgressDrawable.centerRadius = 30f
+    circularProgressDrawable.start()
+    Glide.with(imageView.context).load(url).placeholder(circularProgressDrawable)
+        .error(com.google.android.material.R.drawable.mtrl_ic_error).into(imageView)
+}
